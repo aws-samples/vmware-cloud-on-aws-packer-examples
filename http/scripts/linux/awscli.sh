963edf262d31d1ce1218b3d4b213f16afc3895f2
Troy Lindsay <tlindsay42@users.noreply.github.com>
2020-09-27 00:22:47 -0500

    Move AWS CLI prerequisites to provisioner script

--- http/scripts/linux/awscli.sh
+++ http/scripts/linux/awscli.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+/usr/bin/printf "Installing AWS CLI prerequisites...\n"
+/usr/bin/sudo /usr/bin/apt-get install --quiet --yes python3-pip unzip
+
 /usr/bin/printf "Installing AWS CLI...\n"
 PATH="$HOME/.local/bin:$PATH" /usr/bin/pip3 install --quiet awscli
 
